8cc0ebe64af6e28098d0ce9342864d020ab1efb5
Alld4y <mhek33529@gmail.com>
2026-02-09 18:20:16 +0700

    Feature: delete orderProcess function

--- calculator.cpp
+++ calculator.cpp
@@ -14,16 +14,7 @@ int main() {
     return 0;
 }
 
-void orderProcess(Cal data) {
-    for(int i = 0 ; i < data.opCount ; i++) {
-        if(data.op[i] == '+' && data.op[i+1] != '*') {
-
-        }
-    }
-}
-
 Cal inputEvaluate(char *input) {
-    
     int index = 0;
     int startIntNumber = 0;
     int numberCount = 0;
