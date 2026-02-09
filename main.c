9593c2818cc28c177b9e3eeb38172389fbcdc7cc
Monthon Sova <igaccmonthon1150@gmail.com>
2026-02-09 17:53:53 +0700

    Setup main program structure and separate header/source files

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct Cal {
+    double *numArr;
+    char   *op;
+    int     numCount;
+    int     opCount;
+} Cal;
+
+
+Cal   inputEvaluate(char *input);
+void  orderProcess(Cal data);
+double evaluate(Cal cal);
+
+int main(void) {
+
+    printf("test");
+    return 0;
+}
+
+Cal inputEvaluate(char *input) {
+  
+    Cal evaluated = {0};
+    return evaluated;
+}
+
+void orderProcess(Cal data) {
+   
+}
+
+double evaluate(Cal cal) {
+    
+    return 0.0;
+}
